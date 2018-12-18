c38205cc951f11a911914435dfcfb10a13d98cac
Miles Budden <miles@budden.net>
2018-12-18 11:56:52 +0000

    Added S#

--- SparseMatrix.java
+++ SparseMatrix.java
@@ -1,9 +1,9 @@
 
-/* Put your student number here
- *
- * Optionally, if you have any comments regarding your submission, put them here. 
- * For instance, specify here if your program does not generate the proper output or does not do it in the correct manner.
- */
+/* C1769331
+*
+* Optionally, if you have any comments regarding your submission, put them here. 
+* For instance, specify here if your program does not generate the proper output or does not do it in the correct manner.
+*/
 
 import java.util.*;
 import java.io.*;
@@ -11,24 +11,24 @@ import java.io.*;
 // A class that represents a dense vector and allows you to read/write its elements
 class DenseVector {
 	private int[] elements;
-
+	
 	public DenseVector(int n) {
 		elements = new int[n];
 	}
-
+	
 	public DenseVector(String filename) {
 		File file = new File(filename);
 		ArrayList<Integer> values = new ArrayList<Integer>();
-
+		
 		try {
 			Scanner sc = new Scanner(file);
-
+			
 			while (sc.hasNextInt()) {
 				values.add(sc.nextInt());
 			}
-
+			
 			sc.close();
-
+			
 			elements = new int[values.size()];
 			for (int i = 0; i < values.size(); ++i) {
 				elements[i] = values.get(i);
@@ -37,28 +37,28 @@ class DenseVector {
 			e.printStackTrace();
 		}
 	}
-
+	
 	// Read an element of the vector
 	public int getElement(int idx) {
 		return elements[idx];
 	}
-
+	
 	// Modify an element of the vector
 	public void setElement(int idx, int value) {
 		elements[idx] = value;
 	}
-
+	
 	// Return the number of elements
 	public int size() {
 		return (elements == null) ? 0 : (elements.length);
 	}
-
+	
 	// Print all the elements
 	public void print() {
 		if (elements == null) {
 			return;
 		}
-
+		
 		for (int i = 0; i < elements.length; ++i) {
 			System.out.println(elements[i]);
 		}
@@ -76,19 +76,19 @@ public class SparseMatrix {
 		System.err.println(" - Add two matrices: java SparseMatrix -a <MatrixFile1> <MatrixFile2>");
 		System.err.println(" - Matrix-vector multiplication: java SparseMatrix -v <MatrixFile> <VectorFile>");
 	}
-
+	
 	public static void main(String[] args) throws Exception {
 		if (args.length < 2) {
 			printCommandError();
 			System.exit(-1);
 		}
-
+		
 		if (args[0].equals("-i")) {
 			if (args.length != 2) {
 				printCommandError();
 				System.exit(-1);
 			}
-
+			
 			SparseMatrix mat = new SparseMatrix();
 			mat.loadEntries(args[1]);
 			System.out.println("Read matrix from " + args[1]);
@@ -99,7 +99,7 @@ public class SparseMatrix {
 				printCommandError();
 				System.exit(-1);
 			}
-
+			
 			SparseMatrix mat = new SparseMatrix();
 			mat.loadEntries(args[1]);
 			System.out.println("Read matrix from " + args[1] + ":");
@@ -109,7 +109,7 @@ public class SparseMatrix {
 				printCommandError();
 				System.exit(-1);
 			}
-
+			
 			SparseMatrix mat = new SparseMatrix();
 			mat.loadEntries(args[1]);
 			System.out.println("Read matrix from " + args[1]);
@@ -125,13 +125,13 @@ public class SparseMatrix {
 				printCommandError();
 				System.exit(-1);
 			}
-
+			
 			SparseMatrix mat1 = new SparseMatrix();
 			mat1.loadEntries(args[1]);
 			System.out.println("Read matrix 1 from " + args[1]);
 			System.out.println("Matrix elements:");
 			mat1.print();
-
+			
 			System.out.println();
 			SparseMatrix mat2 = new SparseMatrix();
 			mat2.loadEntries(args[2]);
@@ -139,22 +139,22 @@ public class SparseMatrix {
 			System.out.println("Matrix elements:");
 			mat2.print();
 			SparseMatrix mat_sum1 = mat1.add(mat2);
-
+			
 			System.out.println();
 			mat1.multiplyBy(2);
 			SparseMatrix mat_sum2 = mat1.add(mat2);
-
+			
 			mat1.multiplyBy(5);
 			SparseMatrix mat_sum3 = mat1.add(mat2);
-
+			
 			System.out.println("Matrix1 + Matrix2 =");
 			mat_sum1.print();
 			System.out.println();
-
+			
 			System.out.println("Matrix1 * 2 + Matrix2 =");
 			mat_sum2.print();
 			System.out.println();
-
+			
 			System.out.println("Matrix1 * 10 + Matrix2 =");
 			mat_sum3.print();
 		} else if (args[0].equals("-v")) {
@@ -162,45 +162,45 @@ public class SparseMatrix {
 				printCommandError();
 				System.exit(-1);
 			}
-
+			
 			SparseMatrix mat = new SparseMatrix();
 			mat.loadEntries(args[1]);
 			DenseVector vec = new DenseVector(args[2]);
 			DenseVector mv = mat.multiply(vec);
-
+			
 			System.out.println("Read matrix from " + args[1] + ":");
 			mat.print();
 			System.out.println();
-
+			
 			System.out.println("Read vector from " + args[2] + ":");
 			vec.print();
 			System.out.println();
-
+			
 			System.out.println("Matrix-vector multiplication:");
 			mv.print();
 		}
 	}
-
+	
 	// Loading matrix entries from a text file
 	// You need to complete this implementation
 	public void loadEntries(String filename) {
 		File file = new File(filename);
-
+		
 		try {
 			Scanner sc = new Scanner(file);
 			numRows = sc.nextInt();
 			numCols = sc.nextInt();
 			entries = new ArrayList<Entry>();
-
+			
 			while (sc.hasNextInt()) {
 				// Read the row index, column index, and value of an element
 				int row = sc.nextInt();
 				int col = sc.nextInt();
 				int val = sc.nextInt();
-
+				
 				// Add your code here to add the element into data member entries
 			}
-
+			
 			// Add your code here for sorting non-zero elements
 			
 			
@@ -211,95 +211,95 @@ public class SparseMatrix {
 			entries = null;
 		}
 	}
-
+	
 	// Default constructor
 	public SparseMatrix() {
 		numRows = 0;
 		numCols = 0;
 		entries = null;
 	}
-
+	
 	// A class representing a pair of column index and elements
 	private class Entry {
 		private int position; // Position within row-major full array representation
 		private int value; // Element value
-
+		
 		// Constructor using the column index and the element value
 		public Entry(int pos, int val) {
 			this.position = pos;
 			this.value = val;
 		}
-
+		
 		// Copy constructor
 		public Entry(Entry entry) {
 			this(entry.position, entry.value);
 		}
-
+		
 		// Read column index
 		int getPosition() {
 			return position;
 		}
-
+		
 		// Set column index
 		void setPosition(int pos) {
 			this.position = pos;
 		}
-
+		
 		// Read element value
 		int getValue() {
 			return value;
 		}
-
+		
 		// Set element value
 		void setValue(int val) {
 			this.value = val;
 		}
 	}
-
+	
 	// Adding two matrices
 	public SparseMatrix add(SparseMatrix M) {
 		// Add your code here
 	}
-
+	
 	// Transposing a matrix
 	public SparseMatrix transpose() {
 		// Add your code here
 	}
-
+	
 	// Matrix-vector multiplication
 	public DenseVector multiply(DenseVector v) {
 		// Add your code here
 	}
-
+	
 	// Return the number of non-zeros
 	public int numNonZeros() {
 		// Add your code here
 	}
-
+	
 	// Multiply the matrix by a scalar, and update the matrix elements
 	public void multiplyBy(int scalar) {
 		// Add your code here
 	}
-
+	
 	// Number of rows of the matrix
 	public int getNumRows() {
 		return this.numRows;
 	}
-
+	
 	// Number of columns of the matrix
 	public int getNumColumns() {
 		return this.numCols;
 	}
-
+	
 	// Output the elements of the matrix, including the zeros
 	// Do not modify this method
 	public void print() {
 		int n_elem = numRows * numCols;
 		int pos = 0;
-
+		
 		for (int i = 0; i < entries.size(); ++i) {
 			int nonzero_pos = entries.get(i).getPosition();
-
+			
 			while (pos <= nonzero_pos) {
 				if (pos < nonzero_pos) {
 					System.out.print("0 ");
@@ -307,25 +307,25 @@ public class SparseMatrix {
 					System.out.print(entries.get(i).getValue());
 					System.out.print(" ");
 				}
-
+				
 				if ((pos + 1) % this.numCols == 0) {
 					System.out.println();
 				}
-
+				
 				pos++;
 			}
 		}
-
+		
 		while (pos < n_elem) {
 			System.out.print("0 ");
 			if ((pos + 1) % this.numCols == 0) {
 				System.out.println();
 			}
-
+			
 			pos++;
 		}
 	}
-
+	
 	private int numRows; // Number of rows
 	private int numCols; // Number of columns
 	private ArrayList<Entry> entries; // Non-zero elements
